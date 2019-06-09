083396dc62b0a6845fdc0e947dee9fb5ecf186bc
MatheusNicolielo <matheusnicolielo@gmail.com>
2019-06-09 12:14:58 -0300

    Aep_2-2

---
+++ Aep_2-2/Aep_2-2/src/main/java/Aep_2_2/Aep_2_2/App.java
@@ -0,0 +1,7 @@
+package Aep_2_2.Aep_2_2;
+
+public class App {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ Aep_2-2/Aep_2-2/src/main/java/Aep_2_2/Aep_2_2/CriarConexao.java
@@ -0,0 +1,18 @@
+package Aep_2_2.Aep_2_2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class CriarConexao {
+	private CriarConexao(){
+		
+	}
+	public static Connection conexao() throws Exception {
+		Connection conn = DriverManager.getConnection(
+				"jdbc:postgresql://localhost:5432/3esoft2019",
+				"postgres",
+				"unicesumar");
+		return conn;
+	}
+}
